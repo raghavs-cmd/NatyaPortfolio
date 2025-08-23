8f5d18c6b7f5f0defef0c4c723c53e6b51008146
raghavs-cmd <raghav.s@lingotran.com>
2025-08-23 22:03:16 +0530

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -32,7 +32,7 @@ const About: React.FC = () => {
                           4. If your image has a different name, just update the path below.
                         */}
                         <img
-                            src="/assets/natya.jpeg"
+                            src="public/natya.jpeg"
                             alt="Natya Sowparnika"
                             className="w-full h-full rounded-full object-cover border-4 border-accent/30 shadow-lg"
                         />
